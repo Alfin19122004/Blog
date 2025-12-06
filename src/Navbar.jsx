6cfa07b5033d46b4592a7d2c51ca2082e2e1c087
Alfin19122004 <alfinblesson19@gmail.com>
2025-12-06 23:33:04 +0530

    Fix GitHub Pages base path

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 
 // import heroImg from "./assets/Logo12345.png";
 
-export default function Sidebar({ minimal = true }) {
+export default function Sidebar({ minimal = true }) 
+{
   const [menuOpen, setMenuOpen] = useState(false);
   const toggleMenu = () => setMenuOpen(!menuOpen);
   const closeMenu = () => setMenuOpen(false);
